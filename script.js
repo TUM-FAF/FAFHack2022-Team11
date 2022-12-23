3f1d143ad1327dc954f626d17cc0efe6bd42896a
Cristian Brinza <cristian.brinza.service.acc@gmail.com>
2022-12-23 18:23:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var kindadatabase ='';
+var  usernamekeep='';
+var dataazi='';
+var spawnusernamekeep = document.getElementById('usernamekeep');
 function show(shown, hidden) {
     document.getElementById(shown).style.display='block';
     document.getElementById(hidden).style.display='none';
@@ -16,9 +19,15 @@ function show(shown, hidden) {
             function (data) {
           var student = '';
           console.log(data);
+         
           
           $.each(data, function (key, value) {
 if (htmlEncode($("#password").val())==value.password &&htmlEncode($("#username").val())==value.username){
+  
+  usernamekeep=value.name+" "+value.surname;
+  spawnusernamekeep.textContent = usernamekeep;
+  document.getElementById("userimage").src=value.avatar;
+
   show('Page2','Page1');
 }
             
@@ -36,7 +45,33 @@ if (htmlEncode($("#password").val())==value.password &&htmlEncode($("#username")
 
 document.addEventListener('DOMContentLoaded', function () {
 
-  show('Page1','Page2');
+
+ // document.getElementById("sel2").style.display='none';
+
+
+
+  addtasks();
+
+
+  var today = new Date(); // yyyy-mm-dd
+
+  // Getting short month name (e.g. "Oct")
+  var month = today.toLocaleString('default', { month: 'long' });
+  
+
+  let currentDatey = new Date().toJSON().slice(0, 4);
+  let currentDated = new Date().toJSON().slice(8, 10);
+  dataazi= currentDatey+ " "+ month+", "+currentDated
+  var element1 = document.getElementById("timetoday");
+        element1.innerHTML =dataazi;
+
+
+
+
+
+
+        
+  show('Page2','Page1');
   var modeSwitch = document.querySelector('.mode-switch');
 
   modeSwitch.addEventListener('click', function () {                     document.documentElement.classList.toggle('dark');
@@ -69,3 +104,41 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.messages-section').classList.remove('show');
   });
 });
+
+
+
+function addtasks(){
+
+
+  $.getJSON("https://62adc88a645d00a28aff9ee5.mockapi.io//tasks", 
+  function (data) {
+var student = '';
+console.log(data);
+
+
+$.each(data, function (key, value) {
+  student+='<div class="message-box">';
+  //CONSTRUCTION OF ROWS HAVING
+  // DATA FROM JSON OBJECT
+  
+  student += '<img src="'+value.avatar+'" alt="profile image">'+
+  '<div class="message-content"> <div class="message-header">';
+
+  student += '<div class="name">'+value.name+'</div>';
+student+='<div class="star-checkbox">'+
+              '<input type="checkbox" id="star-1">'+
+              '<label for="star-1">'+
+                '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">'+
+                  '<polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" /></svg>'+
+              '</label>'+
+            '</div>'+
+          '</div>';
+          student+=' <p class="message-line"> '+value.text+'</p>';
+          let c1= value.date.slice(0, 10);
+          student+=' <p class="message-line time"> '+c1+'</p></div> </div>';
+});
+
+$('#aaaaaa').append(student);
+  });
+
+}
